fcbeebc833add87ab4020618269009c0e00138b4
zachystuff <zachyusuf@live.com>
2022-01-26 16:40:31 -0600

    Added Character Class, AB#23, AB#255

--- src/com/nimble_four/AirMarshal/Character.java
+++ src/com/nimble_four/AirMarshal/Character.java
@@ -1,4 +1,32 @@
 package com.nimble_four.AirMarshal;
 
 class Character {
+    public String name;
+    public String dialog;
+
+    public Character(String name, string dialog) {
+        this.name = name;
+        this.dialog = dialog;
+    }
+
+    public String getItem() {
+        return "Collected Item!"
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", dialog='" + dialog + '\'' +
+                '}';
+    }
 }
