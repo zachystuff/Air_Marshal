376d69ca5ec572c9be6cbb6a87aab08e546139bf
Sheila Cartagena <93410083+scartagena23@users.noreply.github.com>
2022-01-26 17:29:56 -0500

    Updated game class to add prompter and updated main class to execute game(), #AB283, #AB290

--- src/com/nimble_four/AirMarshal/controller/Game.java
+++ src/com/nimble_four/AirMarshal/controller/Game.java
@@ -1,7 +1,32 @@
 package com.nimble_four.AirMarshal.controller;
 
-class Game {
-    public static void start(){
+import com.apps.util.Prompter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Game {
+
+    private Prompter prompter = new Prompter(new Scanner(System.in));
+
+    public void execute() {
+        startGame();
+    }
+
+    public void startGame() {
+        try {
+            Files.readAllLines(Path.of("data/game_intro")).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Play");
+        String test = prompter.prompt("Please enter yes if you see this? ", "yes", "Invalid choice: enter yes");
+
+        if (test.equals("yes")) {
+            System.out.println("Successful");
+        }
     }
 }
